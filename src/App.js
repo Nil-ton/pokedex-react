6d603b939ea6fc1401e43af7924d8b0e5b757d80
Nilton Oliveira <nilton.oliveira0305@gmail.com>
2021-10-01 19:12:24 -0300

    v1

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import 'semantic-ui-css/semantic.min.css'
+import Pokemons from "./components/pokemons"
+import { Icon, Input } from 'semantic-ui-react'
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="conteinerPokedex">
+        <h1>Pokedex</h1>
+        <Input
+          icon={<Icon name='search' inverted circular link />}
+          placeholder='Search...'
+          className="searchPokedex"
+        />
+      </div>
+      <Pokemons />
+    </>
   );
 }
 

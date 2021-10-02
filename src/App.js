a072f4b5b36124c4bcba6031c096cabbfa4c4aae
Nilton Oliveira <nilton.oliveira0305@gmail.com>
2021-10-02 02:14:34 -0300

    finish

--- src/App.js
+++ src/App.js
@@ -1,9 +1,47 @@
 import 'semantic-ui-css/semantic.min.css'
-import Pokemons from "./components/pokemons"
 import { Icon, Input } from 'semantic-ui-react'
-
+import { useEffect, useState } from 'react/cjs/react.development'
+import axios from "axios"
 
 function App() {
+
+  const [pokemons, setPokemons] = useState([])
+  const [search, setSearch] = useState('')
+
+  
+
+  useEffect(() => {
+    const pokemonList = []
+    for (let i = 1; i <= 150; i++) {
+      pokemonList.push(axios.get(`https://pokeapi.co/api/v2/pokemon/${i}`)
+        .then(response => response.data)
+      )
+    }
+
+    Promise.all(pokemonList)
+      .then(response => {
+        setPokemons(response)
+      })
+
+  }, [])
+
+  const filterPokemons = pokemons.filter((pokemon) => pokemon.name.includes(search.toLowerCase()))
+  const showPokemons = filterPokemons.map(
+    (pokemon) =>
+      <ul key={pokemon.name}>
+        <li>
+          <h1>{`#${pokemon.id} ${pokemon.name}`}</h1>
+          <img src={pokemon.sprites.front_default} alt={pokemon.name} />
+          <div className="stats">
+            <h4>{`HP:   ${pokemon.stats[0].base_stat}`}</h4>
+            <h4>{`ATT:  ${pokemon.stats[1].base_stat}`}</h4>
+            <h4>{`DEF:  ${pokemon.stats[2].base_stat}`}</h4>
+            <h4>{`TYPE: ${pokemon.types[0].type.name}`}</h4>
+          </div>
+        </li>
+      </ul>
+  )
+
   return (
     <>
       <div className="conteinerPokedex">
@@ -12,9 +50,13 @@ function App() {
           icon={<Icon name='search' inverted circular link />}
           placeholder='Search...'
           className="searchPokedex"
+          value={search}
+          onChange={(e) => { setSearch(e.target.value)}}
         />
+        <div className="showPokemons">
+          {showPokemons}
+        </div>
       </div>
-      <Pokemons />
     </>
   );
 }
